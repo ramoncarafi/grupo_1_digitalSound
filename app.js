e65ca439016fffe6d9b294c9f1dce4577c1cb96a
Juan Manuel Fernandez <juanmafc22@gmail.com>
2022-09-26 09:02:50 -0300

    App.js modificada, agregadas nuevas rutas /index.html y /. Agregado carrito.html y carrito-styles.css

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const serverPort = 3030;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3030, () => {
-    console.log("Servidor funcionando")
+app.listen(serverPort, () => {
+    console.log("Servidor funcionando OK en el puerto", serverPort)
+    console.log("Ir al sitio Web de Digital Sound: http://localhost:" + serverPort + "/" )
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+})
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
 app.get('/login', (req, res) => {
@@ -14,4 +24,8 @@ app.get('/login', (req, res) => {
 
 app.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, './views/registro.html'))
+})
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/carrito.html'))
 })
