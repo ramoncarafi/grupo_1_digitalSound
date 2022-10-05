9d6afe80a0d645f03abdff119375f2ca1917ee18
Juan Manuel Fernandez <juanmafc22@gmail.com>
2022-10-05 17:42:06 -0300

    Agregados varios ';' al app.js

--- app.js
+++ app.js
@@ -6,33 +6,30 @@ const serverPort = 3030;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(serverPort, () => {
-    console.log("Servidor funcionando OK en el puerto", serverPort)
-    console.log("Ir al sitio Web de Digital Sound: http://localhost:" + serverPort + "/" )
+    console.log("Servidor funcionando OK en el puerto", serverPort);
+    console.log("Ir al sitio Web de Digital Sound: http://localhost:" + serverPort + "/");
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
+    res.sendFile(path.join(__dirname, './views/index.html'));
 })
 
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'))
+    res.sendFile(path.join(__dirname, './views/index.html'));
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'))
+    res.sendFile(path.join(__dirname, './views/login.html'));
 })
 
 app.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/registro.html'))
+    res.sendFile(path.join(__dirname, './views/registro.html'));
 })
 
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito.html'))
+    res.sendFile(path.join(__dirname, './views/carrito.html'));
 })
 
 app.get('/detalle-de-producto', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/detalle-de-producto.html'))
+    res.sendFile(path.join(__dirname, './views/detalle-de-producto.html'));
 })
-
-
-
